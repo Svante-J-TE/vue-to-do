f1b93214180d7f142668960048c0676d8ac58ff4
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2021-02-02 14:09:35 +0100

    simple todo list added

---
+++ todo/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <div>
+      <input type="text" id="todoInput">
+      <button @click="addTodoItem()">Add to list</button>
+    </div>
+    <div v-for="item in todoItems" :key="item.todo">
+      <TodoList :title="item.todo"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import TodoList from './components/TodoList'
+
+export default {
+  name: 'App',
+  components: {
+    TodoList
+  },
+    data(){
+    return{
+      todoItems: []
+    }
+  },
+  methods: {
+    addTodoItem(){
+      this.todoItems.push(
+        {todo: document.getElementById("todoInput").value}
+      );
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ todo/src/components/TodoList.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>{{title}}</h1>
+</template>
+
+<script>
+export default {
+  name: "TodoList",
+  props: {
+    title: String
+  }
+};
+</script>
+
+<style scoped>
+</style>
